6d13b0dea0a2bc565bcbf37e0205bf77d8f55906
Mike Shelby <mikegshelby@gmail.com>
2021-06-12 14:11:35 -0500

    added styles for viewing on mobile devices/smaller screens

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -6,8 +6,8 @@ import NavBanner from '../NavBanner';
 function Banner() {
   return (
           <div className="banner-section">
+                <TitleLogo></TitleLogo>
                 <div className="banner-textbox">
-                        <TitleLogo></TitleLogo>
                         <h2 className="banner-textbox-title">Full Stack Web Developer</h2>
                         <NavBanner></NavBanner>
                 </div>
