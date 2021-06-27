80edc61ac4eae452746fb367a2e7c7241c360a0d
Mike Shelby <mikegshelby@gmail.com>
2021-06-27 16:15:51 -0500

    re-added portfolio-react to path names for GitHub deployment

--- src/App.js
+++ src/App.js
@@ -19,72 +19,72 @@ import ContactDetails from './components/ContactDetails';
 
 function App() {
   return (
-    <React.Fragment>
-      <Home></Home>
-    </React.Fragment>
+    // <React.Fragment>
+    //   <Home></Home>
+    // </React.Fragment>
 
-    // <Router>
-    //     <Switch>
-    //     <Route
-    //         exact
-    //         path="/portfolio-react"
-    //         render={() => {
-    //             return (
-    //               <Redirect to="/home" />
-    //             )
-    //         }}
-    //       />
-    //     <Route
-    //         exact
-    //         path="/"
-    //         render={() => {
-    //             return (
-    //               <Redirect to="/home" />
-    //             )
-    //         }}
-    //     />
-    //     <Route
-    //         exact
-    //         path="."
-    //         render={() => {
-    //             return (
-    //               <Redirect to="/home" />
-    //             )
-    //         }}
-    //     />
-    //     {/* <Route
-    //         exact
-    //         path="/portfolio-react/about"
-    //         render={() => {
-    //             return (
-    //               <Redirect to="/about" />
-    //             )
-    //         }}
-    //     />
-    //     <Route
-    //         exact
-    //         path="/portfolio-react/projects"
-    //         render={() => {
-    //             return (
-    //               <Redirect to="/projects" />
-    //             )
-    //         }}
-    //     />
-    //     <Route
-    //         exact
-    //         path="/portfolio-react/contact"
-    //         render={() => {
-    //             return (
-    //               <Redirect to="/contact" />
-    //             )
-    //         }}
-    //     /> */}
-    //       <Route exact path="/home" component={Home} />
-    //       <Route exact path="/about" component={AboutDetails} />
-    //       <Route exact path="/projects" component={ProjectsAll} />
-    //       <Route exact path="/contact" component={ContactDetails} />
-    //     </Switch>
-    //   </Router>
+    <Router>
+        <Switch>
+        <Route
+            exact
+            path="/portfolio-react"
+            render={() => {
+                return (
+                  <Redirect to="/portfolio-react/home" />
+                )
+            }}
+          />
+        <Route
+            exact
+            path="/"
+            render={() => {
+                return (
+                  <Redirect to="/portfolio-react/home"  />
+                )
+            }}
+        />
+        <Route
+            exact
+            path="."
+            render={() => {
+                return (
+                  <Redirect to="/portfolio-react/home"  />
+                )
+            }}
+        />
+        <Route
+            exact
+            path="/portfolio-react/about"
+            render={() => {
+                return (
+                  <Redirect to="/about" />
+                )
+            }}
+        />
+        <Route
+            exact
+            path="/portfolio-react/projects"
+            render={() => {
+                return (
+                  <Redirect to="/projects" />
+                )
+            }}
+        />
+        <Route
+            exact
+            path="/portfolio-react/contact"
+            render={() => {
+                return (
+                  <Redirect to="/contact" />
+                )
+            }}
+        />
+          <Route exact path="/portfolio-react/home" component={Home} />
+          <Route exact path="/about" component={AboutDetails} />
+          <Route exact path="/projects" component={ProjectsAll} />
+          <Route exact path="/contact" component={ContactDetails} />
+        </Switch>
+      </Router>
   );
 }
 
