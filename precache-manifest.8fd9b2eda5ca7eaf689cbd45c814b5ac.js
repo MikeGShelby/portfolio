7175a5d8ab939fb17e1eba35f2e476f6bac4ec97
Mike Shelby <mikegshelby@gmail.com>
2021-06-27 16:19:41 -0500

    Updates

---
+++ precache-manifest.8fd9b2eda5ca7eaf689cbd45c814b5ac.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e76c796903e42a2b0cae762e400806",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "4d5244a41e081f6e701a",
+    "url": "/portfolio-react/static/css/main.8da83195.chunk.css"
+  },
+  {
+    "revision": "4901b66067d119ff2f4a",
+    "url": "/portfolio-react/static/js/2.6a90377c.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/portfolio-react/static/js/2.6a90377c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d5244a41e081f6e701a",
+    "url": "/portfolio-react/static/js/main.b5dc9468.chunk.js"
+  },
+  {
+    "revision": "2f26d0aed242c262185b",
+    "url": "/portfolio-react/static/js/runtime-main.a4de92d3.js"
+  },
+  {
+    "revision": "87d994875c22d91270c9d27302a45fb7",
+    "url": "/portfolio-react/static/media/bootstrap.87d99487.png"
+  },
+  {
+    "revision": "75fdae4ab914fbf1cce4e814a0c29d18",
+    "url": "/portfolio-react/static/media/budget-tracker.75fdae4a.jpg"
+  },
+  {
+    "revision": "b5485f3123c0fdf565e0d5d211781650",
+    "url": "/portfolio-react/static/media/bulma.b5485f31.png"
+  },
+  {
+    "revision": "aaede29da0e0107507fc8f5623496922",
+    "url": "/portfolio-react/static/media/cinasearch.aaede29d.jpg"
+  },
+  {
+    "revision": "e63112d51560b2b93dea4397657d8037",
+    "url": "/portfolio-react/static/media/diet-delivery.e63112d5.jpg"
+  },
+  {
+    "revision": "3969b17fe51497816ed23b07055805fb",
+    "url": "/portfolio-react/static/media/fit-club.3969b17f.jpg"
+  },
+  {
+    "revision": "583dca4c9bef88de713b67b46ddd252f",
+    "url": "/portfolio-react/static/media/galaxy-bg.583dca4c.jpg"
+  },
+  {
+    "revision": "b3dc8c0956346a747b1f3b5f01812d18",
+    "url": "/portfolio-react/static/media/moon.b3dc8c09.png"
+  },
+  {
+    "revision": "58c77fb828ed1f9286c104ef550a0736",
+    "url": "/portfolio-react/static/media/password-generator.58c77fb8.jpg"
+  },
+  {
+    "revision": "020e80ec696f6e10fd6671c3d8edd365",
+    "url": "/portfolio-react/static/media/photoshop.020e80ec.png"
+  },
+  {
+    "revision": "84b3491704c37a58b766177f230f4666",
+    "url": "/portfolio-react/static/media/placeholder-3.84b34917.jpg"
+  },
+  {
+    "revision": "407acf142cd7094d110eb15e3b42dff6",
+    "url": "/portfolio-react/static/media/placeholder-4.407acf14.jpg"
+  },
+  {
+    "revision": "1f118aef772ffd6ceddff84db5b1d687",
+    "url": "/portfolio-react/static/media/placeholder-5.1f118aef.jpg"
+  },
+  {
+    "revision": "790a08d8035c8845006f0c8000c680ad",
+    "url": "/portfolio-react/static/media/resume.790a08d8.pdf"
+  },
+  {
+    "revision": "1c50b14e32136ecb4eabdb59301eceb8",
+    "url": "/portfolio-react/static/media/run-buddy.1c50b14e.jpg"
+  },
+  {
+    "revision": "358c52daeae8a75c573d9f7dc92873d6",
+    "url": "/portfolio-react/static/media/self-image.358c52da.jpg"
+  },
+  {
+    "revision": "a40313a28a33701791193a014ccee974",
+    "url": "/portfolio-react/static/media/taskinator.a40313a2.jpg"
+  },
+  {
+    "revision": "888d03af0da223397a7468d4d4948d99",
+    "url": "/portfolio-react/static/media/tech-blog.888d03af.jpg"
+  },
+  {
+    "revision": "f665cf9b5269ed504cf6d84d77678c6d",
+    "url": "/portfolio-react/static/media/weather-dashboard.f665cf9b.jpg"
+  },
+  {
+    "revision": "94c5435ae874e5256180c27a9290a0f6",
+    "url": "/portfolio-react/static/media/work-day-scheduler.94c5435a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6f4a51d052a5b47803ed50b1b93810d1.js"
+  "/portfolio-react/precache-manifest.8fd9b2eda5ca7eaf689cbd45c814b5ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
