3a26e92f93ba4302b60b86abe1823524727bae13
Mike Shelby <mikegshelby@gmail.com>
2021-06-29 08:10:02 -0500

    removed featured projects from container

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,13 +4,11 @@ import Project from '../Project';
 function Projects() {
   return (
     <section class="project-section bg-light">
-      <div className="container">
         <h3 id="projects" class="section-title project-section-title text-center text-dark">My Projects</h3>
         <div className="featured-projects">
           <Project></Project>
         </div>
         <a href="#/projects" className="btn-grad btn-small btn-projects">SEE MORE OF MY WORK</a>
-      </div>
     </section>
   );
 }
